61a983ee04fac6c64ccdb660e250c8e9bf7a8ff5
MukeshOm <mukesh.dommati92@gmail.com>
2016-10-23 20:31:23 -0500

    Added files

---
+++ app.js
@@ -0,0 +1,11 @@
+var to = "a todo";
+var nam = "Mukesh";
+var todo = {
+    id: 12,
+    nam1: "test",
+    salary: 20000
+};
+var id = todo.id, nam1 = todo.nam1, salary = todo.salary;
+var list1 = [3, 4, 5];
+var list2 = [1, 2, 3];
+list1.push.apply(list1, list2);
---
+++ app.ts
@@ -0,0 +1,11 @@
+let to:string = "a todo";
+let nam:string = "Mukesh";
+let todo =  {
+    id: 12,
+    nam1: "test",
+    salary : 20000
+}
+let {id,nam1,salary} = todo ;
+let list1 = [3,4,5];
+let list2 = [1,2,3];
+list1.push(...list2);
